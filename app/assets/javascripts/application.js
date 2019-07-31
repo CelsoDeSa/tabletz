4266c025555af5475b099578be81404f2b4897cb
Celso de Sa <730584+CelsoDeSa@users.noreply.github.com>
2019-07-31 11:54:16 -0300

    Add admin and bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,10 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+
+// Bootstrap Stuff
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+
 //= require_tree .
