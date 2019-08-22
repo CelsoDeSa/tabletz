40a7a7f2a15e91db45bb6ef2f5fed937a88f6cbe
Celso de Sa <730584+CelsoDeSa@users.noreply.github.com>
2019-08-22 15:20:04 -0300

    Added image modifier before saving

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
 class Post < ApplicationRecord
     has_and_belongs_to_many :reviews, -> { order('updated_at desc') }
+    validates :title, :url, :pic, presence: true
+    validates :url, uniqueness: true
+    before_save :modify_pic
+
+    protected
+ 
+    def modify_pic
+        self.pic.gsub!(/("_blank")/, "\"_blank\" rel=\"nofollow\"")
+    end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,12 @@
 class Review < ApplicationRecord
     has_and_belongs_to_many :posts
+    validates :title, :url, :pic, presence: true
+    validates :url, uniqueness: true
+    before_save :modify_pic
 
-    # def to_param
-    #     device
-    # end
+    protected
+ 
+    def modify_pic
+        self.pic.gsub!(/("_blank")/, "\"_blank\" rel=\"nofollow\"")
+    end
 end
