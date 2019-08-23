13443d4342ef89be59ea8f31ff0b84031301c99f
Celso de Sa <730584+CelsoDeSa@users.noreply.github.com>
2019-08-23 10:06:31 -0300

    Image showing and nofollow formating, also other minimal layout changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,14 @@
 class Post < ApplicationRecord
     has_and_belongs_to_many :reviews, -> { order('updated_at desc') }
-    validates :title, :url, :pic, presence: true
+    validates :title, :url, presence: true
     validates :url, uniqueness: true
     before_save :modify_pic
 
     protected
  
     def modify_pic
-        self.pic.gsub!(/("_blank")/, "\"_blank\" rel=\"nofollow\"")
+        if self.pic && !self.pic.include?("rel=\"nofollow\"")
+            self.pic.gsub!(/("_blank")/, "\"_blank\" rel=\"nofollow\"")
+        end        
     end
 end
